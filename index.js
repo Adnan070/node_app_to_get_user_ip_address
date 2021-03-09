8b4114847473a6f5ac7d5d9719b2b9074877d835
msajid123 <msajidmehmood602@gmail.com>
2021-03-09 16:59:16 +0500

    using ipware package

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(function (req, res, next) {
+  var ipInfo = getIP(req);
+  console.log(ipInfo);
+  req.ipInfo = ipInfo;
+  // { clientIp: '127.0.0.1', clientIpRoutable: false }
+  next();
+});
 
 app.get("/api", (req, res) => {
   res.json({
